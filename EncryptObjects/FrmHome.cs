701a6b8e019e0b5bfe0eedd540f250d9e7062bc0
Nabi Karampoor <thisisnabi@outlook.com>
2022-06-27 23:56:45 +0430

    create project

--- EncryptObjects/FrmHome.cs
+++ EncryptObjects/FrmHome.cs
@@ -6,5 +6,45 @@ namespace EncryptObjects
         {
             InitializeComponent();
         }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
